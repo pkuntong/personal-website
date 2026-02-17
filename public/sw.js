97bdd371f6dbbd57f8421829580f699cfcc778ca
pkuntong <paudcin@gmail.com>
2026-02-16 19:13:42 -0500

    Fix MIME type errors on site

--- public/sw.js
+++ public/sw.js
@@ -1,44 +1,13 @@
-const CACHE_NAME = 'pau-kuntong-v2';
-const urlsToCache = [
-  '/',
-  '/blog',
-  '/projects',
-  '/src/main.tsx',
-  '/src/App.tsx',
-  '/src/pages/Index.tsx',
-  '/src/index.css'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-      )
-  );
-});
-
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches
+      .keys()
+      .then((cacheNames) => Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   );
 });
